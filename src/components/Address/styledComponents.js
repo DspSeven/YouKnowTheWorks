063fbb42b7eb609de8453ab7df45cc00d14939b6
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-04-20 11:03:46 +0530

    Create styledComponents.js

---
+++ src/components/Address/styledComponents.js
@@ -0,0 +1,23 @@
+import Styled from 'styled-components'
+
+export const PersonAddress = Styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+line-height: 15px;`
+
+export const AddressDetails = Styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+line-height: 10px;`
+
+export const Disclaimer = Styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;`
+
+export const Info = Styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;`
